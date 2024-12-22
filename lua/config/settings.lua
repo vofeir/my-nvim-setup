b39b8fae334550375a9211aefe26e03ca6d2dcab
Christian Giraudo <vofeir@gmail.com>
2024-12-21 22:46:12 -0600

    gitsigns options
    Without signs column, color for line numbers

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -5,3 +5,11 @@ opt.linebreak = true
 opt.number = true
 opt.shiftwidth = 4
 opt.softtabstop = 4
+
+-- gitsigns plugin colors:
+vim.api.nvim_set_hl(0, 'GitSignsAdd',    
+    { fg = '#00FF00', bg = 'NONE' }) -- Verde para adiciones
+vim.api.nvim_set_hl(0, 'GitSignsChange', 
+    { fg = '#FFFF00', bg = 'NONE' }) -- Amarillo para cambios
+vim.api.nvim_set_hl(0, 'GitSignsDelete', 
+    { fg = '#FF0000', bg = 'NONE' }) -- Rojo para eliminaciones

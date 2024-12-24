2ee41b4ff6d4c6e1b60a246ee795fa61e6872396
Christian Giraudo <vofeir@gmail.com>
2024-12-23 23:38:20 -0600

    highlight options

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,4 +1,5 @@
 local opt = vim.o
+local hl = vim.api.nvim_set_hl
 
 opt.termguicolors = true -- permite colores hex
 opt.expandtab = true
@@ -9,14 +10,14 @@ opt.softtabstop = 4
 
 -- Margen derecho:
 opt.colorcolumn = "80"
-vim.api.nvim_set_hl(0, 'ColorColumn', { bg = '#070712' })
+hl(0, 'ColorColumn', { bg = '#070712' })
 
 -- gitsigns plugin colors
 --   verde para adiciones:
-vim.api.nvim_set_hl(0, 'GitSignsAdd', { fg = '#00FF00', bg = 'NONE' })
+hl(0, 'GitSignsAdd', { fg = '#00FF00', bg = 'NONE' })
 
 --   amarillo para cambios:
-vim.api.nvim_set_hl(0, 'GitSignsChange', { fg = '#FFFF00', bg = 'NONE' })
+hl(0, 'GitSignsChange', { fg = '#FFFF00', bg = 'NONE' })
 
 --   rojo para eliminaciones:
-vim.api.nvim_set_hl(0, 'GitSignsDelete', { fg = '#FF0000', bg = 'NONE' })
+hl(0, 'GitSignsDelete', { fg = '#FF0000', bg = 'NONE' })
